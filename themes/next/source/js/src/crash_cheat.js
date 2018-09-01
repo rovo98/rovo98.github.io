edaf34ef3e4e5af3b9630635711dd93cfd215e5d
rovo98 <2402323937@qq.com>
2018-09-01 12:29:46 +0800

    update hexo source files at 2018.9.1

---
+++ themes/next/source/js/src/crash_cheat.js
@@ -0,0 +1,18 @@
+ <!--崩溃欺骗-->
+ <!-- added by rovo98 -->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/images/my-favicon-16x16.png");
+         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/images/my-favicon-32x32.png");
+         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
+ });
