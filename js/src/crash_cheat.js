e7c5d725fc3611265f48d52ff7aea54a56a0e234
rovo98 <rovo98@qq.com>
2020-05-17 18:33:29 +0800

    Site updated: 2020-05-17 18:33:27

---
+++ js/src/crash_cheat.js
@@ -0,0 +1,19 @@
+ <!--崩溃欺骗-->
+ <!-- added by rovo98 -->
+ var OriginTitle = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/images/favicon-sad.png");
+         document.title = '页面崩溃啦 ~' + OriginTitle;
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/images/favicon-happy.png");
+         document.title = '噫又好了~' + OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+             $('[rel="icon"]').attr('href', '/images/my-favicon-32x32.png');
+         }, 2000);
+     }
+ });
